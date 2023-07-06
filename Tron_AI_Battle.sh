131cd5972309ebd91cd7f771a9e1da874e6ec716
B-Wass <101531474+B-Wass@users.noreply.github.com>
2023-07-06 16:26:58 -0400

    Add files via upload

--- Tron_AI_Battle.sh
+++ Tron_AI_Battle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DISPLAY=:0 nohup python3 /home/pi/tron_ai_battle/Tron_AI_Battle.py >/dev/null 2>&1 &
+DISPLAY=:0 nohup python3 $(find / -name "Tron_AI_Battle.py" 2>/dev/null) >/dev/null 2>&1 &
